46e9451284b796ef61a597f54e1fef1a0d8eca70
Stanislav Tonkich <st.tonkikh@gmail.com>
2023-03-24 18:33:16 +0100

    update 2403

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -128,7 +128,7 @@ export default function Settings() {
               />
             </Item>
             <Item>
-              <Label>Auto start Pomodoros?</Label>
+              <Label>Auto start StenPomodoros?</Label>
               <Switch
                 on={autoPomodoros}
                 onClick={() => dispatch(toggleAutoPomodoros())}
